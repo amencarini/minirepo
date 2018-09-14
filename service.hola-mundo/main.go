819db3fd03a11fd1c7129a650c3ed7cfcb3e0a96
Alessandro Mencarini <alessandromencarini@monzo.com>
2018-09-14 19:38:32 +0100

    Adapt to new structure

--- service.hola-mundo/main.go
+++ service.hola-mundo/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "¡Hola mundo!")
+func salutation() string {
+	return "¡Hola mundo!"
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	for {
+		log.Printf(salutation())
+		time.Sleep(1 * time.Second)
+	}
 }
--- service.hola-mundo/main_test.go
+++ service.hola-mundo/main_test.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"net/http/httptest"
 	"testing"
 )
 
-// A small addition just to trigger tests...
 func TestSaysHelloWorld(t *testing.T) {
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", bytes.NewReader([]byte{}))
-	handler(w, r)
-	result := w.Body.String()
+	result := salutation()
 	expected := "¡Hola mundo!"
 	if result != expected {
 		t.Errorf("Expected %s, got %s", expected, result)
