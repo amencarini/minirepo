70cc59310dcb467f545ac49dccb8cd89ed4ab428
Alessandro Mencarini <alessandromencarini@monzo.com>
2018-09-21 16:09:51 +0100

    Add baaaaaa

---
+++ service.random/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSaysHelloWorld(t *testing.T) {
+	result := salutation()
+	expected := "Baaaaaaah"
+	if result != expected {
+		t.Errorf("Expected %s, got %s", expected, result)
+	}
+}
